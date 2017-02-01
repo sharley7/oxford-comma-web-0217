41947cc947a250ed2a288fb8641399a05cf1ba02
S. Marie Kav <siobhan.harley@gmail.com>
2017-02-01 16:19:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   if array.count == 1
-  array.join(",")
+  array.join(',')
 elsif array.count == 2
-  array.join(" and ")
+  array.join(' and ')
 elsif array.count >= 3
   array[0...-1].join(', ') + ', and ' + array.last
 end
